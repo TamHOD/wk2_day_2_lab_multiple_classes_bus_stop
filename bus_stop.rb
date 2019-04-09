6ad18ab1d33e37d620421a273b332111e074657e
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-09 16:00:10 +0100

    finish all

--- bus_stop.rb
+++ bus_stop.rb
@@ -1,5 +1,21 @@
-require("minitest/autorun")
-require("minitest/rg")
-require_relative("../bus_stop")
 
-class BusStopTest < MiniTest::Test
+
+class BusStop
+
+  attr_reader :name, :queue
+
+  def initialize(name)
+    @name = name
+    @queue = []
+  end
+
+  def add_person(name)
+    @queue.push(name)
+  end
+
+  def pickup_all(bus, stop)
+    bus.pickup(@queue)
+    @queue = []
+  end
+
+end
---
+++ specs/bus_stop_spec.rb
@@ -0,0 +1,42 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../bus_stop")
+require_relative("../person")
+require_relative("../bus")
+
+
+class BusStopTest < MiniTest::Test
+
+
+  def setup
+    @bob = Person.new("Bob", "28")
+    @bill = Person.new("Bill", "33")
+    @barbara = Person.new("Barbara", "25")
+    @brenda = Person.new("Brenda", "77")
+
+
+    @bus33 = Bus.new("33", "Mayfield")
+
+    @stop1 = BusStop.new("Dalkeith")
+  end
+
+  def test_get_stop_name
+    assert_equal("Dalkeith", @stop1.name)
+  end
+
+  def test_add_person_to_queue
+    @stop1.add_person(@bob)
+    assert_equal(1, @stop1.queue.length)
+  end
+
+  def test_pickup_all_from_stop
+    @stop1.add_person(@bob)
+    @stop1.add_person(@brenda)
+    @stop1.add_person(@bill)
+    @stop1.add_person(@barbara)
+    @stop1.pickup_all(@bus33, @stop1)
+    assert_equal(0, @stop1.queue.length)
+    assert_equal(4, @bus33.passengers.length)
+  end
+
+end
